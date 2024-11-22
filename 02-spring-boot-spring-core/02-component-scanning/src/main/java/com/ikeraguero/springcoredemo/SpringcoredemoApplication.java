5967f89872e4d231b56b3b82f1af73705012d20a
ikeraguero <ikerpires407@gmail.com>
2024-11-22 08:26:56 -0300

    Add Spring Boot Core

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/ikeraguero/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,19 @@
+package com.ikeraguero.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+		scanBasePackages = {"com.ikeraguero.springcoredemo," +
+							"com.ikeraguero.util"})
+ */
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/test/java/com/ikeraguero/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikeraguero.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
